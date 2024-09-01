1127c6f629449c6ee5b8a6870f08348f89693f08
Ankit Kumar Dam <aparnadam1981@gmail.com>
2024-09-01 11:30:02 +0530

    m\

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -45,7 +45,7 @@ export const Pokemon = () => {
       .includes(search.toUpperCase())
   );
 
-  const ac = searchType === "name" ? searchData : searchData1;
+  const ac = searchType == "type" ? searchData1 : searchData;
 
   if (loading) {
     return (
